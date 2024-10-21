89a88d5b7f8e3632f7f1f9b7c8b2120faf4aeeec
Schuh <500190471@fvtc.edu>
2024-10-21 16:24:27 -0500

    Completed Exam 2

--- site.js
+++ site.js
@@ -11,11 +11,22 @@ const nextQuestionElement = document.querySelector('#nextQuestion')
 ;(async () => {
 
 	// todo: create your "getNextQuestion" function
+	// const getNextQuestion = async url => {
+		const getNextQuestion = await fetch('https://opentdb.com/api.php?amount=1&category=21&difficulty=easy&type=multiple')
+		const json = await getNextQuestion.json()
+		
+		const { question, correct_answer: correct, incorrect_answers: incorrect } = json.results[0]
+		const answers = shuffle([ ...incorrect, correct ])
+		return { question, answers, correct }
+		console.log(await getNextQuestion())
+	// }
+	
+	
 
 	// todo: create your "renderQuestion" function
 
 	// todo: add the event listener to the "nextQuestion" button
-
+	
 })()
 
 // mimic a click on the "nextQuestion" button to show the first question
